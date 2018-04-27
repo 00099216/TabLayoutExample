2c9db7de63b6fa7db6bb2301a224caeefb1b70fb
Alejandro <alejandrovelasco94@gmail.com>
2018-04-27 18:10:45 -0500

    added ViewPager to manage TabLayout Fragments

---
+++ app/src/main/java/com/velasco00036514/navigationproject/RestaurantListFragment.java
@@ -0,0 +1,6 @@
+package com.velasco00036514.navigationproject;
+
+import android.support.v4.app.Fragment;
+
+public class RestaurantListFragment extends Fragment {
+}
---
+++ app/src/main/java/com/velasco00036514/navigationproject/RestaurantPagerAdapter.java
@@ -0,0 +1,6 @@
+package com.velasco00036514.navigationproject;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class RestaurantPagerAdapter extends FragmentPagerAdapter {
+}
