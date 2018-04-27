3952131ff23f948888972ee41669ae14025e328e
Alejandro <alejandrovelasco94@gmail.com>
2018-04-27 16:39:44 -0500

    initial commit, added few elements to main layout

---
+++ app/src/main/java/com/velasco00036514/navigationproject/MainActivity.java
@@ -0,0 +1,21 @@
+package com.velasco00036514.navigationproject;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setting up the toolbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
