0c808199530f46bece3467c8c374722b5845c9b7
Alejandro <alejandrovelasco94@gmail.com>
2018-05-01 15:26:24 -0500

    added savedinstancestate method implementation

--- app/src/main/java/com/velasco00036514/navigationproject/activities/MainActivity.java
+++ app/src/main/java/com/velasco00036514/navigationproject/activities/MainActivity.java
@@ -1,15 +1,16 @@
 package com.velasco00036514.navigationproject.activities;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.velasco00036514.navigationproject.R;
-import com.velasco00036514.navigationproject.Restaurant;
 import com.velasco00036514.navigationproject.adapters.RestaurantPagerAdapter;
 import com.velasco00036514.navigationproject.fragments.RestaurantListFragment;
+import com.velasco00036514.navigationproject.models.Restaurant;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,25 +24,45 @@ public class MainActivity extends AppCompatActivity implements RestaurantListFra
     RestaurantListFragment restaurantFragment, favoriteFragment;
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putParcelableArrayList("restaurantList", (ArrayList<? extends Parcelable>) restaurantList);
+
+        //saving fragments instance
+        if (restaurantFragment.isAdded())
+            getSupportFragmentManager().putFragment(outState, "restaurantFragment", restaurantFragment);
+
+        if (favoriteFragment.isAdded())
+            getSupportFragmentManager().putFragment(outState, "favoriteFragment", favoriteFragment);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null || !savedInstanceState.containsKey("instance")){
+            //setting up initial list
+            restaurantList = fillRestaurants();
+
+            //creating fragments
+            restaurantFragment = new RestaurantListFragment();
+            favoriteFragment = new RestaurantListFragment();
+
+            //configuring fragments
+            restaurantFragment.setList(restaurantList);
+            favoriteFragment.setList(favRestaurants(restaurantList));
+        } else {
+            restaurantList = savedInstanceState.getParcelableArrayList("instance");
+            restaurantFragment = (RestaurantListFragment) getSupportFragmentManager().getFragment(savedInstanceState, "restaurantFragment");
+            favoriteFragment = (RestaurantListFragment) getSupportFragmentManager().getFragment(savedInstanceState, "favoriteFragment");
+        }
 
         //setting up the Toolbar
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
-
-        //setting up initial list
-        restaurantList = fillRestaurants();
-
-        //creating fragments
-        restaurantFragment = new RestaurantListFragment();
-        favoriteFragment = new RestaurantListFragment();
-
-        //configuring fragments
-        restaurantFragment.setList(restaurantList);
-        favoriteFragment.setList(favRestaurants(restaurantList));
 
         //setting up the PagerAdapter
         pagerAdapter = new RestaurantPagerAdapter(getSupportFragmentManager(), this);
